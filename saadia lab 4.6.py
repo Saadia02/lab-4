f8122d173e2a63fa0353c6ec02eaa6be215bee0c
Saadia02 <141850384+Saadia02@users.noreply.github.com>
2023-10-25 00:58:05 -0700

    Add files via upload

---
+++ saadia lab 4.6.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from skimage import io
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
+import numpy as np
+from skimage.filters import threshold_otsu
+from scipy.stats import linregress
+import glob
+import math
+
+time = 0
+time_list = []
+area_list = []
+
+# Update the path and pattern based on your file structure
+path = "D:\image processing\myenv\Lab-4\*.*"
+
+# Print the files returned by glob.glob() for debugging
+files = glob.glob(path)
+print("Files found:", files)
+
+num_images = len(files)
+
+# Calculate the number of rows and columns for the subplot grid
+num_cols = 2  # Display each image and its thresholded version side by side
+num_rows = math.ceil(num_images / num_cols)
+
+# Create a figure and a grid of subplots
+fig, axes = plt.subplots(num_rows, num_cols, figsize=(10, 15))
+for idx, file in enumerate(files):
+    img = io.imread(file)
+    entropy_img = entropy(img, disk(3))
+    thresh = threshold_otsu(entropy_img)
+    binary = entropy_img <= thresh
+    scratch_area = np.sum(binary == 1)
+    print("time=", time, "hr  ", "Scratch area=", scratch_area, "pix²")
+    time_list.append(time)
+    area_list.append(scratch_area)
+    time += 1
+
+    # Calculate the subplot index
+    row_idx = idx // num_cols
+    col_idx = idx % num_cols
+
+    # Display the original image in the left subplot
+    axes[row_idx, col_idx * 2].imshow(img, cmap='gray')
+    axes[row_idx, col_idx * 2].set_title(f'Time = {time} hr\nOriginal Image')
+
+    # Display the thresholded image in the right subplot with a different color
+    axes[row_idx, col_idx * 2 + 1].imshow(binary, cmap='coolwarm', interpolation='none')
+    axes[row_idx, col_idx * 2 + 1].set_title('Thresholded Image')
+
+# Adjust layout and display the figure
+plt.tight_layout()
+plt.show()
+
+plt.plot(time_list, area_list, 'bo')  # Print blue dots scatter plot
+slope, intercept, r_value, p_value, std_err = linregress(time_list, area_list)
+print("y = ", slope, "x", " + ", intercept)
+print("R² = ", r_value**2)
+plt.show()
